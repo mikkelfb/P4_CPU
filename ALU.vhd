ac6a1573a1170c00359fef93003a7d7c604d6fc4
mikkelfb <mbengt17@student.aau.dk>
2020-04-23 11:16:35 +0200

    Added-ALU-testbench

--- ALU.vhd
+++ ALU.vhd
@@ -1,34 +1,28 @@
 library IEEE;
 Use IEEE.STD_LOGIC_1164.ALL;
-Use IEEE.std_logic_arith.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+Use IEEE.NUMERIC_STD.all;
 
 entity ALU is
 	port (
 		opCode						:	in STD_LOGIC_VECTOR(4 downto 0);
 		ALUA							:	in STD_LOGIC_VECTOR(15 downto 0);
 		ALUB							:	in STD_LOGIC_VECTOR(15 downto 0);
-		ALUOut						:	in STD_LOGIC_VECTOR(15 downto 0);
-		EnAlu							: 	in STD_LOGIC;
-		carryFlag, zeroFlag		: 	in STD_LOGIC
+		ALUOut						:	out STD_LOGIC_VECTOR(15 downto 0);
+		EnAlu							: 	in STD_LOGIC
+		--carryFlag, zeroFlag		: 	out STD_LOGIC
 	);
 end ALU;
-
 
 architecture Behavioral of ALU is
 	signal aluRes	: std_logic_vector(15 downto 0);
 begin
 
-	
-	
-	
-	
-	
-
 process (EnAlu, opCode, ALUA , ALUB)
 	begin
 		if(EnAlu = '1') then
 			case opCode is
-				when "00010" => --Write to SRAM
+				when "00010" => --Write to SRAMC:\intelFPGA_lite\18.1\modelsim_ase\win32aloem
 					aluRes <= (ALUA);
 				when "00011" => -- A Bigger then B
 					if(ALUA > ALUB) then 
@@ -53,12 +47,12 @@ process (EnAlu, opCode, ALUA , ALUB)
 				when "00111" => -- A bitwise OR B
 					aluRes <= ALUA OR ALUB;
 				when "01000" =>  -- A + B
-					aluRes <= ALUA + ALUB;
+					aluRes <= std_logic_vector(to_unsigned(( to_integer(unsigned(ALUA)) + to_integer(unsigned(ALUB))), 16)); --OLD: ALUA + ALUB;
 				when "01001" =>  -- A - B
-					aluRes <=ALUA - ALUB;
+					aluRes <= std_logic_vector(to_unsigned(( to_integer(unsigned(ALUA)) - to_integer(unsigned(ALUB))), 16)); --OLD: ALUA - ALUB;
 				when "01010" =>  -- A / B
 					aluRes <= std_logic_vector(to_unsigned(( to_integer(unsigned(ALUA)) / to_integer(unsigned(ALUB))), 16));
-				when "01011" => -- A * BaluResaluRes
+				when "01011" => -- A * B
 					aluRes <= std_logic_vector(to_unsigned(( to_integer(unsigned(ALUA)) * to_integer(unsigned(ALUB))), 16));
 				when "01110" => --Write to UART
 					aluRes <= (ALUA);
@@ -68,9 +62,12 @@ process (EnAlu, opCode, ALUA , ALUB)
 					aluRes <= (ALUA);
 				when others =>
 					aluRes <= x"0000";
-		end if
+			end case;
+		else
+			aluRes <= "ZZZZZZZZZZZZZZZZ";
+		end if;
 end process;
 
-ALUOut <= aluRes
+ALUOut <= aluRes;
 
 end Behavioral;
---
+++ tb_ALU.vhd
@@ -0,0 +1,166 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+use IEEE.std_logic_unsigned.all;
+
+
+Entity tb_ALU IS
+END tb_ALU;
+
+Architecture behavior of tb_ALU IS
+
+	COMPONENT ALU
+	PORT(
+		opCode						:	in STD_LOGIC_VECTOR(4 downto 0);
+		ALUA							:	in STD_LOGIC_VECTOR(15 downto 0);
+		ALUB							:	in STD_LOGIC_VECTOR(15 downto 0);
+		ALUOut						:	out STD_LOGIC_VECTOR(15 downto 0);
+		EnAlu							: 	in STD_LOGIC
+		--carryFlag, zeroFlag		: 	out STD_LOGIC
+	);
+	END COMPONENT;
+	
+	signal ALUA 	: STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
+   signal ALUB 	: STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
+   signal opCode 	: STD_LOGIC_VECTOR(4 downto 0) := (others => '0');
+	signal EnAlu	: STD_LOGIC;
+
+  --Outputs
+   signal ALUOut : STD_LOGIC_VECTOR(15 downto 0);
+	
+	BEGIN
+	
+	uut: ALU PORT MAP(
+		ALUA 		=> ALUA,
+		ALUB 		=> ALUB,
+		ALUOut 	=> ALUOut,
+		opCode 	=> opCode,
+		EnAlu 	=> EnAlu
+	);
+	
+	process
+	begin
+		
+		-- Test OPCODE 00010 -> letting A pass 
+		ALUA <= x"00AA";
+		ALUB <= x"00BB";
+		opCode <= "00010";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 00011 -> A > B -> 1
+		ALUA <= x"00AA";
+		ALUB <= x"0011";
+		opCode <= "00011";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 00011 -> A > B -> 0
+		ALUA <= x"0011";
+		ALUB <= x"00BB";
+		opCode <= "00011";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 00100 -> A < B -> 1
+		ALUA <= x"0011";
+		ALUB <= x"00BB";
+		opCode <= "00100";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 00100 -> A < B -> 0
+		ALUA <= x"00AA";
+		ALUB <= x"0011";
+		opCode <= "00100";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		
+		-- Test OPCODE 00101 -> A = B -> 1
+		ALUA <= x"0011";
+		ALUB <= x"0011";
+		opCode <= "00101";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 00101 -> A = B -> 0
+		ALUA <= x"00AA";
+		ALUB <= x"0011";
+		opCode <= "00101";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 00110 -> A bitwise AND B
+		ALUA <= x"00FF";
+		ALUB <= x"FFFF";
+		opCode <= "00110";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 00111 -> A bitwise OR B
+		ALUA <= x"0F0F";
+		ALUB <= x"F00F";
+		opCode <= "00111";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 01000 -> addition 1 + 1 (HALLO WORLD!)
+		ALUA <= x"0001";
+		ALUB <= x"0001";
+		opCode <= "01000";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 01001 -> subtraction 2 - 1 
+		ALUA <= x"0005";
+		ALUB <= x"0001";
+		opCode <= "01001";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		
+		-- Test OPCODE 01010 -> division 6 / 2 = 3
+		ALUA <= x"0006";
+		ALUB <= x"0002";
+		opCode <= "01010";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 01011 -> multiplication 6 * 2 = 12
+		ALUA <= x"0006";
+		ALUB <= x"0002";
+		opCode <= "01011";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 01110 -> Write to UART let A through
+		ALUA <= x"0006";
+		ALUB <= x"0002";
+		opCode <= "01110";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 01111 -> Write to 7-segment let A through
+		ALUA <= x"0006";
+		ALUB <= x"0002";
+		opCode <= "01111";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test OPCODE 10000 -> Write to SD let A through
+		ALUA <= x"0006";
+		ALUB <= x"0002";
+		opCode <= "10000";
+		EnAlu <= '1';
+		wait for 10 ns;
+		
+		-- Test EnALu disable
+		ALUA <= x"0006";
+		ALUB <= x"0002";
+		opCode <= "00001";
+		EnAlu <= '0';
+		wait for 10 ns;
+		
+		
+	end process;
+end behavior;
